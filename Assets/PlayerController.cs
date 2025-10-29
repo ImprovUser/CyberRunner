401352dc89650016a5a00f495b86c2740fba3cbd
Max <60143006+ImprovUser@users.noreply.github.com>
2025-10-29 16:45:05 -0400

    Jumping

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -22,8 +22,22 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float fallMultiplier = 3.5f;
     [SerializeField] private float lowJumpMultiplier = 5f;
 
+    [Header("Wall Check")]
+    [SerializeField] private Transform wallCheckLeft;
+    [SerializeField] private Transform wallCheckRight;
+    [SerializeField] private float wallCheckDistance = 0.1f;
+    [SerializeField] private LayerMask wallLayer;
+    [SerializeField] private float wallSlideSpeed = 0.5f;
+    [SerializeField] private float wallJumpPush = 10f;
+    [SerializeField] private float wallCheckRadius = 0.1f;
+
+
+    private bool isTouchingWallLeft;
+    private bool isTouchingWallRight;
     private float jumpTimeCounter;
     private bool isJumping;
+    private float wallJumpGraceTime = 0.2f;
+    private float lastWallTime;
 
     void Start()
     {
@@ -34,12 +48,21 @@ public class PlayerController : MonoBehaviour
     {
         GetControls();
         CheckJump();
+        CheckWallTouch();
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Debug.Log("Manual Wall Jump Test");
+            WallJump();
+        }
+
     }
-    
+
     void FixedUpdate()
     {
         Move();
-        ApplyGravityControl() ;
+        WallSlide();
+        ApplyGravityControl();
     }
 
     void GetControls()
@@ -47,18 +70,23 @@ public class PlayerController : MonoBehaviour
         xAxis = Input.GetAxisRaw("Horizontal");
     }
 
-    private void Move()
+    void CheckJump()
     {
-        rb.linearVelocity = new Vector2(walkSpeed * xAxis, rb.linearVelocity.y);
-    }
-
-    void CheckJump ()
-    {
-        if(Input.GetButtonDown("Jump") && isGrounded())
+        if (Input.GetButtonDown("Jump") && (isGrounded() || Time.time - lastWallTime < wallJumpGraceTime))
         {
             isJumping = true;
             jumpTimeCounter = jumpTimeMax;
-            Jump();
+
+            if (IsOnWall() && !isGrounded())
+            {
+                // If we're on a wall, perform a wall jump (push away horizontally + vertical)
+                WallJump();
+            }
+            else
+            {
+                // Normal ground jump
+                Jump();
+            }
         }
 
         if(Input.GetButton("Jump") && isJumping)
@@ -80,6 +108,51 @@ public class PlayerController : MonoBehaviour
             isJumping = false;
         }
     }
+
+    void CheckWallTouch()
+    {
+        // Debug ray draw
+        Debug.DrawRay(wallCheckLeft.position, Vector2.left * wallCheckDistance, Color.red);
+        Debug.DrawRay(wallCheckRight.position, Vector2.right * wallCheckDistance, Color.blue);
+
+        isTouchingWallLeft = Physics2D.OverlapCircle(wallCheckLeft.position, wallCheckRadius, wallLayer);
+        isTouchingWallRight = Physics2D.OverlapCircle(wallCheckRight.position, wallCheckRadius, wallLayer);
+    }
+
+
+    private void Move()
+    {
+        rb.linearVelocity = new Vector2(walkSpeed * xAxis, rb.linearVelocity.y);
+    }
+
+    private void WallSlide()
+    {
+        bool pushingIntoLeftWall = isTouchingWallLeft && xAxis < 0;
+        bool pushingIntoRightWall = isTouchingWallRight && xAxis > 0;
+
+        if ((pushingIntoLeftWall || pushingIntoRightWall) && !isGrounded() && rb.linearVelocity.y < 0)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, Mathf.Max(rb.linearVelocity.y, -wallSlideSpeed));
+        }
+    }
+
+    private void WallJump()
+    {
+        float horizontalDir = 0f;
+
+        if (isTouchingWallLeft) horizontalDir = 1f;
+        else if (isTouchingWallRight) horizontalDir = -1f;
+
+        Debug.Log($"Wall Jump! Dir: {horizontalDir}, TouchLeft: {isTouchingWallLeft}, TouchRight: {isTouchingWallRight}");
+
+        // Failsafe: if no wall detected, abort
+        if (horizontalDir == 0f)
+            return;
+
+        rb.linearVelocity = new Vector2(horizontalDir * wallJumpPush, jumpForce);
+    }
+
+
     private void Jump()
     {
         rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
@@ -114,5 +187,26 @@ public class PlayerController : MonoBehaviour
             rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.fixedDeltaTime;
         }
     }
+
+    private bool IsOnWall()
+    {
+        return isTouchingWallLeft || isTouchingWallRight;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (wallCheckLeft != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(wallCheckLeft.position, wallCheckLeft.position + Vector3.left * wallCheckDistance);
+        }
+
+        if (wallCheckRight != null)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(wallCheckRight.position, wallCheckRight.position + Vector3.right * wallCheckDistance);
+        }
+    }
+
 
 }
